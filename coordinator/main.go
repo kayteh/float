dcf35200491a6ff46ae32c1b227651d7a4dca661
Katalina / stardust <git@kat.cafe>
2017-07-29 21:47:15 -0500

    documentation pass

--- coordinator/main.go
+++ coordinator/main.go
@@ -1,3 +1,10 @@
+// The coordinator's job is to manage kubernetes resources and resolve routes.
+// This can ideally be scaled to multiple containers if load gets too high.
+// Coordinator results might be cached on the gateway, however that might be a bad idea.
+// This is one of two long-running services to facilitate serverless architecture.
+//
+// It should be considered this could be consolidated into the gateway and made serverless itself.
+// I'm not very sure of the downsides of this approach.
 package main
 
 import (
@@ -7,6 +14,7 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
+// This is straight-forward. Create a server, run the server.
 func main() {
 	r := fasthttprouter.New()
 
@@ -19,7 +27,13 @@ func main() {
 	s.ListenAndServe(":5345")
 }
 
+// handleRouteInfo is a WIP function.
+// This route will take a POST /route-info, match it against some rules,
+// e.g. if $Header matches /regex/, match to function X()
+// And return (and possibly start) a container ready to serve it.
 func handleRouteInfo(ctx *fasthttp.RequestCtx) {
+	// TODO: grab routes from a database (postgres?)
+	// TODO: match route-info data against some rules.
 	log.Println("route-info call")
 	ctx.WriteString(`{"addr": "localhost:7955"}`)
 }
--- gateway/run/server.go
+++ gateway/run/server.go
@@ -1,3 +1,5 @@
+// Package run is the physical gateway server.
+// This is separated so the run-package as a whole is testable e2e.
 package run
 
 import (
@@ -9,12 +11,15 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
+// Server is a struct for the gateway and everything it needs to operate.
 type Server struct {
 	CoordinatorAddr string
 	Logger          *logrus.Entry
 	Client          *fasthttp.Client
 }
 
+// routeInfoRequest is an outgoing POST body to <coordinator>/route-info
+// Everything in this struct should be considered for routing.
 type routeInfoRequest struct {
 	Path       []byte
 	Host       []byte
@@ -23,10 +28,12 @@ type routeInfoRequest struct {
 	Headers    *fasthttp.RequestHeader
 }
 
+// routeInfoResponse is everything <coordinator>/route-info will ever return.
 type routeInfoResponse struct {
 	Addr string
 }
 
+// Start is straight-forward. Start the
 func (s *Server) Start() {
 	if s.Client == nil {
 		s.Client = &fasthttp.Client{}
@@ -39,6 +46,15 @@ func (s *Server) Start() {
 	srv.ListenAndServe(":3491")
 }
 
+// director has a simple but tedious job,
+// * get the route-info response,
+// * reverse proxy the request into where route-info goes.
+// This involves copying most of the fasthttp.RequestCtx.
+//
+// As an impl note: net/http/httputil#ReverseProxy was not ideal for the job.
+// Copying headers, post-body, and other various pieces wasn't straight-forward
+// Even editing the request in-flight just to re-call was difficult.
+// I personally prefer the simplicity of fasthttp, so that's why it's used here.
 func (s *Server) director(ctx *fasthttp.RequestCtx) {
 
 	// first, take relevant parts of the request
